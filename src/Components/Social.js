4e8d2cbebb5d8ad20c4d2ecf15e9806ef6f3db90
PrateekGoel97 <43164503+PrateekGoel97@users.noreply.github.com>
2022-01-27 14:03:52 +0530

    added social icons

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,11 +1,27 @@
+import {SocialData} from '../data/social';
 
 
 const Social = (props) =>{
 
+    const data = SocialData;
 
+    console.log(data[0].icon);
     return(
-        <div classname="social">
-            this is social
+        <div className="social-contact">
+            {
+                data.map((item) =>{
+                    return (
+                        <a href={item.link}>
+
+                            <div className="social-icon-div">
+                                
+                                <img src={item.icon} className="social-icon" alt="social-icon" ></img>
+                            </div>
+
+                        </a>
+                    )
+                })
+            }
         </div>
     )
 }
---
+++ src/data/social.js
@@ -0,0 +1,25 @@
+
+import github from '../images/github.png';
+import linkedin from '../images/linkedin.png';
+import twitter from '../images/twitter.png';
+
+
+
+export const SocialData = [
+    {
+        platform:"Twitter",
+        icon:twitter,
+        link:"https://twitter.com/prateek76590397"
+    },
+    {
+        platform:"Github",
+        icon:github,
+        link:"https://github.com/PrateekGoel97"
+    },
+    {
+        platform:"LinkedIn",
+        icon:linkedin,
+        link:"https://www.linkedin.com/in/prateek-goel-566862137/"
+    }
+
+];
